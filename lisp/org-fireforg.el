1a20e71b7592064c43a4460068ff4d8a25257466
Andreas Burtzlaff <andreas@burtzlaff.de>
2009-08-10 20:47:29 +0200

    Fixed reading of org-agenda-files

--- lisp/org-fireforg.el
+++ lisp/org-fireforg.el
@@ -1,8 +1,30 @@
+;;; org-fireforg.el --- provide functionality for interaction of
+;;;                     Fireforg, a Firefox extension, with org mode
 
 
+;; Copyright 2009 Andreas Burtzlaff
+;;
+;; Author: Andreas Burtzlaff < andreas at burtz[REMOVE]laff dot de >
+;; Version: 0.1alpha2
+;; Keywords: org-mode filesystem tree
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-
-
+;;; Commentary:
 
 ;;  Protocol:
 ;;
@@ -39,7 +61,7 @@
 
 
 ;; This needs to be customizable
-(defun org-fireforg-registry-file-set () org-agenda-files)
+(defun org-fireforg-registry-file-set () (org-agenda-files))
 
 (defcustom org-fireforg-registry-file
   (concat (getenv "HOME") "/.org-fireforg-registry.el")
