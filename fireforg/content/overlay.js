547246c269686b16028967b37272c58e69c96034
Andreas Burtzlaff <andreas@burtzlaff.de>
2010-07-19 23:06:42 +0200

    Moved doi related functions to doi.js

--- fireforg/content/overlay.js
+++ fireforg/content/overlay.js
@@ -98,10 +98,10 @@ var fireforg = {
                         //alert("Prefetching link " + url);
                         fireforg.jQuery.get( url, function (htmlText) {
                                 // get doi
-                                var doi = fireforg.getDOIFromHtml( htmlText );
+                                var doi = fireforg_doi.getDOIFromHtml( htmlText );
 
                                 if( doi && doi != "" ) {
-                                    var doiURL = fireforg.doiToURL( doi );
+                                    var doiURL = fireforg_doi.doiToURL( doi );
                                     // Add resolved URL to link map
                                     fireforg.linkMapAddEntry( url, doiURL );
                                     registryEntry = fireforg.getRegistryEntryFromFileForUrl( doiURL );
@@ -197,9 +197,9 @@ var fireforg = {
 		fireforg.currentLinkRegistryEntry = fireforg.getRegistryEntryFromFileForUrl( url );
                 // add all doi matches if enabled
                 if( fireforg.getPreferenceManager().getBoolPref("extensions.fireforg.matchDOI") ) {
-                    var doi = fireforg.getDOIFromHtml( fireforg.jq("html").html() );
+                    var doi = fireforg_doi.getDOIFromHtml( fireforg.jq("html").html() );
                     if( doi ) {
-                        fireforg.currentHeadingsMatchingDOI = fireforg.getRegistryEntryFromFileForUrl( fireforg.doiToURL(doi) );
+                        fireforg.currentHeadingsMatchingDOI = fireforg.getRegistryEntryFromFileForUrl( fireforg_doi.doiToURL(doi) );
                     } else
                       fireforg.currentHeadingsMatchingDOI = null;
                 } else
@@ -738,7 +738,7 @@ var fireforg = {
             // See how the site's content can be accessed.
             // If it is the currently viewed url, simply use the content of the <html> tag to search through
             if( window.content.document.URL == url ) {
- doi = fireforg.getDOIFromHtml( fireforg.jq("html").html() );
+ doi = fireforg_doi.getDOIFromHtml( fireforg.jq("html").html() );
             } 
             // If it isn't the currently viewed URL the content has to
             // be retrieved. But we won't do that here because of its
@@ -746,10 +746,10 @@ var fireforg = {
             // and handled in fireforg.lookupAndModifyLinks.
 
             if( doi ) {
-                registryEntryMapped = fireforg.getRegistryEntryForLink( fireforg.doiToURL(doi) );
+                registryEntryMapped = fireforg.getRegistryEntryForLink( fireforg_doi.doiToURL(doi) );
 
                 // add DOI URL to map
-                fireforg.linkMapAddEntry( url, fireforg.doiToURL( doi ) );
+                fireforg.linkMapAddEntry( url, fireforg_doi.doiToURL( doi ) );
             } 
         } 
 
@@ -820,42 +820,6 @@ var fireforg = {
         //alert("linkToAbsoluteUrl: link = " + link + ", rootDir = " + rootDir + "\n=> " + newLink);
         return rootDir + newPath;
     },
-    /* DOI HANDLING */
-    getDOIFromHtml: function( html ) {
-        // Note: this presumes that "<" and ">" are encoded in the DOI identifier
-        // The DOI specification only _recommends_ "<" and ">" to be encoded inside xml documents.
-        // Let's hope everybody does so...
-        var doiRegexp = /doi ?[:\/] ?([0123456789]+\.[^ \/]+\/[^ <>\"]+)/i;
-        var doiRegexpResult = doiRegexp.exec( html );
-        if( !doiRegexpResult || doiRegexpResult.length < 1 ) {
-            return null;
-        } else {
-            return doiRegexpResult[1];
-        }
-    },
-    doiToURL: function ( string ) {
-        return "http://dx.doi.org/" + string.replace(/%/g,"%25").replace(/"/g,"%22").replace(/#/g,"%23").replace(/ /g,"%20"); // "
-                                                                         },
-        // removes duplicate heading entries that are children to the given root node
-        // simple unoptimized solution
-        removeDuplicateAnnotations: function  ( rootNode ) {
-            
-            var childrenArray = fireforg.jQuery.makeArray( rootNode.children() );
-                        
-            var allChildren = rootNode.children();
-            var uniqueChildren = allChildren;
-            //            var test = fireforg.jQuery.unique( fireforg.jQuery.makeArray( allChildren ) );
-            rootNode = rootNode.empty();
-            allChildren.each( function () {
-                    var currentEntry = this;
-                    uniqueChildren = uniqueChildren.filter( function () { 
-                            return !( (fireforg.jq(this).attr("file") == fireforg.jq(currentEntry).attr("file")
-                                       && (fireforg.jq(this).attr("point") == fireforg.jq(currentEntry).attr("point"))  )) }).add( currentEntry );
+}
 
-                        });
-                return rootNode.empty().append( uniqueChildren );
-        }
-
-        };
-        window.addEventListener("load", function(e) { fireforg.onLoad(e); }, false);
-
+window.addEventListener("load", function(e) { fireforg.onLoad(e); }, false);
